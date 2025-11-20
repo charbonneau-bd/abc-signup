6b783f68d5353cb5ad04eccd72d5f79694524856
charbonneau <charbod@gmail.com>
2025-11-20 13:03:20 +0100

    Initial Creation

---
+++ java/com/example/abc_signup/AbcSingupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abc_signup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcSingupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
