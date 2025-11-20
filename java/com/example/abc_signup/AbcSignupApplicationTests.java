827d58524d1cf4ec19b11cc4a8da1d4f85dec359
charbonneau <charbod@gmail.com>
2025-11-20 14:20:04 +0100

    Initial Creation

--- java/com/example/abc_signup/AbcSignupApplicationTests.java
+++ java/com/example/abc_signup/AbcSignupApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AbcSingupApplicationTests {
+class AbcSignupApplicationTests {
 
 	@Test
 	void contextLoads() {
